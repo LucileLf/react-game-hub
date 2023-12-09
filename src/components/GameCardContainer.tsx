acddd29ded390bb3af06d9a20b5f9e60ed7639a1
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-12-09 12:52:59 +0100

    remove duplicated styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,18 @@
+import { Box } from '@chakra-ui/react'
+import React, { ReactNode } from 'react'
+
+//to avoid duplication of styling GameCard and GameCardSkeleton
+
+interface GameCardContainerProps {
+  children: ReactNode
+}
+
+const GameCardContainer = ({children} : GameCardContainerProps) => {
+  return (
+    <Box width='300px' borderRadius = {10} overflow="hidden">
+      {children}
+    </Box>
+  )
+}
+
+export default GameCardContainer
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='300px'>
-      <Skeleton borderRadius = {10} height="250px" />
+    <Card>
+      <Skeleton height="250px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
